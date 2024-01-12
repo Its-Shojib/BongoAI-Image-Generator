0f02f586f2aed52b6f90e054b2ee04d4a5889438
Its_Shojib <mdshojib922@gmail.com>
2024-01-12 18:25:05 +0600

    Design Implementedd

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Footer from "./Components/Footer"
 import ImageGenerator from "./Components/ImageGenerator"
 
 function App() {
-
   return (
     <div className="min-h-screen">
       <ImageGenerator></ImageGenerator>
